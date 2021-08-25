114b2956645cd7c0db469672730a132d9717bd0d
Rahul <rahulshinde235@gmail.com>
2021-08-25 18:18:54 +0530

    Initial commit v1

---
+++ app.js
@@ -0,0 +1,29 @@
+const correctAnswers=['B','B','B','B'];
+const result=document.querySelector('.result');
+
+const form=document.querySelector('.quiz-form');
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    let score=0;
+    const userAnswers=[form.q1.value,form.q2.value,form.q3.value,form.q4.value];
+    console.log(userAnswers);
+    userAnswers.forEach((answer,index)=>{
+        if(answer===correctAnswers[index]){
+            score+=25;
+        }
+    });
+    //show result on page
+    scrollTo(0,0); //for scrolling to the top of the page
+    result.classList.remove('d-none');
+    let output=0;
+    const timer=setInterval(() => {
+        result.querySelector('span').textContent=`${output}%`;
+        if(output===score){
+            clearInterval(timer);
+        }
+        output++;
+
+
+    }, 10);
+
+});
